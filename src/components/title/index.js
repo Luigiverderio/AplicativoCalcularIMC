833e22d365fcdc800ce73da3b6e36c32770e66d1
Luigi <Luigiverderio@hotmail.com>
2024-02-25 19:15:28 -0300

    Adicionando a logo do SESI

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View , Text} from "react-native";
+import {View , Text, Image } from "react-native";
 import styles from "./style"
+import logoSesi from  "../../components/img/logosesi.jpg"
 
 export default function Title(){
   return(
     <View style={styles.boxTitle}>
+       <Image source={logoSesi} alt="LogodoSESI" style={styles.logo}/>
       <Text style={styles.textTitle}>APP IMC SESI MONTE ALTO</Text>
     </View>
   );
--- src/components/title/style.js
+++ src/components/title/style.js
@@ -3,12 +3,17 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   boxTitle: {
     alignItems:"center",
-    height: '10%',
+    height: '20%',
   },
   textTitle: {
     color:"#FF0043",
     fontSize:24,
     fontWeight:"bold"
+  },
+  logo: {
+    height:70,
+    width:200,
+    marginBottom:50
   }
   });
 
