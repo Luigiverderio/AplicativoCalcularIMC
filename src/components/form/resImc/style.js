b81b4344e7311a39cd583837d7df4fc88388eb49
Luigi <Luigiverderio@hotmail.com>
2024-02-25 18:56:34 -0300

    Finalizando o site

---
+++ src/components/form/resImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  resultImc: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: 'center',
+    width: '100%'
+  },
+  numberImc: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: '#FF0043'
+  },
+  information: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FF0043'
+  }
+});
+
+export default styles
